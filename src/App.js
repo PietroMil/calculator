e6e6320cec17a18db7a0288abad39a0d2c29a6c0
PietroMil <pietro.milanese@edu.itspiemonte.it>
2022-11-24 14:02:17 +0100

    creata app base

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { Operators } from './components/calculator';
 
 function App() {
+  const data= {
+    value: '-',
+    input: 10,
+    active: false
+}
+const data2= {
+    value: '-',
+    input: 10,
+    active: true
+}
+
+const data3= {
+  value: '+',
+  input: 400,
+  active: true
+}
+
+const myNumbers = [data, data2, data3 ]
+
+Operators(myNumbers)
+
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/components/calculator.js
@@ -0,0 +1,30 @@
+
+
+export const Operators = (input) => {
+const myNumbers = []
+//get the numbers
+    input.forEach(element => {
+        const value = (element.value)
+        let input = (element.input)
+        const active = (element.active)
+        
+        if(active === false){
+            input = 0
+        }
+        const number = value + input
+        myNumbers.push(+number)
+        console.log(myNumbers)
+    });
+
+//calculate
+    const initialValue = 0;
+const sumWithInitial = myNumbers.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  initialValue
+);
+
+console.log(sumWithInitial);
+
+    
+}
+
